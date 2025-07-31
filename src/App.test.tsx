7441a04407082825d115448f3492e7656e74a4cf
davidh216 <davidh216@gmail.com>
2025-07-31 19:30:39 -0400

    fix: resolve formatting and test issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders FinanceApp text', () => {
+test('renders FinanceApp', () => {
   render(<App />);
-  const linkElement = screen.getByText(/FinanceApp is Ready/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/FinanceApp is Ready/i);
+  expect(element).toBeInTheDocument();
 });
